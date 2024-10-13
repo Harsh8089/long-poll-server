09e389b157c4f8b8dfb3ec8e84a5c4113f907e79
Harsh Jain <121497446+Harsh8089@users.noreply.github.com>
2024-10-13 18:46:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ setInterval(async () => {
     } catch (error) {
         console.error('Error making request:', error.message);
     }
-}, 10 * 60 * 1000); 
+}, 15000); 
 
 app.get('/', (req, res) => {
     return res.send("Server is running and polling every 5 seconds.");
